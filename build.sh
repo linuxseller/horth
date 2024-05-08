0f61ad22c7609b499af7adbdb0df19011cdc78eb
shibanutyitokom <shibanutyitokom@yandex.ru>
2024-05-08 02:07:43 +0000

    Compiling to ./build/horth

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 set -xe
 
-ghc ./src/Main.hs ./src/Parser.hs -outputdir ./build
+ghc ./src/Main.hs ./src/Parser.hs -outputdir ./build -o ./build/horth
 #rm src/Parser.hi src/Parser.o src/Main.hi src/Main.o
 if [[ $1 == run ]] then
     ./build/horth
