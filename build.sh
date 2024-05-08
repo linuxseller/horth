dacf0d838cae2ebe5bab0d00539eaeb572d4470f
shibanutyitokom <shibanutyitokom@yandex.ru>
2024-05-08 19:48:53 +0000

    Updated ./build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/env bash
 set -xe
 
-ghc ./src/Main.hs ./src/Parser.hs -outputdir ./build -o ./build/horth
+SRC_FILES="./src/Main.hs ./src/Parser.hs ./src/Runner.hs ./src/Data/Memory.hs"
+
+ghc $SRC_FILES -isrc/Data -isrc -outputdir ./build -o ./build/horth
 #rm src/Parser.hi src/Parser.o src/Main.hi src/Main.o
 if [[ $1 == run ]] then
     ./build/horth
