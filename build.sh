3da06f339127fab2c9bc2a0026751201d3e453b2
shibanutyitokom <shibanutyitokom@yandex.ru>
2024-05-09 21:03:08 +0000

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,12 @@
 #!/bin/env bash
 set -xe
-if [[ $1 == compile ]] then
-    as test.asm -g -o test.o
-    ld test.o -o test
-    rm test.o
-    exit
-fi
+
 SRC_FILES="./src/Main.hs ./src/Parser.hs ./src/Runner.hs ./src/Data/Memory.hs ./src/Data/Parser.hs ./src/Compiler.hs"
 
 ghc $SRC_FILES -isrc/Data -isrc -outputdir ./build -o ./build/horth.o
 #rm src/Parser.hi src/Parser.o src/Main.hi src/Main.o
 if [[ $1 == run ]] then
-    ./build/horth.o
+    ./build/horth.o examples/test.horth -m compile -o test
+    ./test
 fi
 
