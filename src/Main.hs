5550679a016b3745be2947dfc4d7cc40f034025d
shibanutyitokom <shibanutyitokom@yandex.ru>
2024-05-09 21:04:29 +0000

    Added compilation argument to Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,8 @@ import Parser
 import Control.Applicative
 import Data.Maybe
 import System.Environment
+import System.Exit
+import System.Process
 
 import Data.Memory
 import Data.Parser
@@ -13,17 +15,43 @@ import Compiler
 
 programToAst code = snd $ fromMaybe ("", []) $ runParser (many $ ws*>parseAst) code
 
+getMode :: [String] -> String
+getMode [] = "compile"
+getMode [_] = "compile"
+getMode (f:m:xs)
+  | f == "-m" = m
+  | otherwise = getMode (m:xs)
 
+getOutfile :: [String] -> String
+getOutfile [] = "a"
+getOutfile [_] = "a"
+getOutfile (f:m:xs)
+  | f == "-o" = m
+  | otherwise = getOutfile (m:xs)
 
 main :: IO ()
 main = do
-  [filename, "-t", mode] <- getArgs
+  -- [filename, "-t", mode] <- getArgs
+  args <- getArgs
+  let mode = getMode args
+  let filename = head args
+  let outfile = getOutfile args
   prg <- readFile $ filename
   let (rest, program) = fromMaybe ("", []) $ runParser (many $ ws*>parseAst) prg
   let validationResult = validateRun program
+  putStrLn "Validating..."
   case validationResult of
-    (Right a) -> print a
-    _ ->
-      case mode of
-        "run" -> putStrLn $ show $ run program
-        "compile" -> putStrLn $ compile program
+    (Right a) -> do
+      print a
+      exitWith $ ExitFailure 1
+    _ -> putStrLn "Validation: success"
+  case mode of
+    "run" -> putStrLn $ show $ run program
+    "compile" -> do
+      writeFile (outfile <> ".asm") $ compile program
+      putStrLn "Compiling..."
+      callProcess "as" [outfile<>".asm", "-o", outfile<>".o"]
+      putStrLn "Linking..."
+      callProcess "ld" [outfile<>".o", "-o", outfile]
+      callProcess "rm" [outfile<>".o"]
+      putStrLn "Done!"
